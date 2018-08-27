a1ffc19209d6ddfa5e656fc3ebe6fe28e74c682b
cleclerc <cleclerc@netatlantic.com>
2018-08-27 14:14:16 -0400

    - Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import database.DBCPDatabase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        //Note Java 8's 'try-catch' automatically closes the connection and statement via an internal 'finally' block
+        try (Connection conn = DBCPDatabase.getDataSource().getConnection();
+             PreparedStatement statement = conn.prepareStatement("UPDATE TestTable SET Username = 'new_username'")) {
+
+            statement.execute();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
 
     }
 
